4dc01da89b3c450b86465405442a6c7c52e8a24f
Sawan Vijayvergiya <Vijayvergiya_Sa@India.XoriantCorp.com>
2021-02-06 10:31:26 +0530

    New work Signed-off-by: Sawan Vijayvergiya <Vijayvergiya_Sa@India.XoriantCorp.com>

---
+++ SparkJavaTest.java
@@ -0,0 +1,53 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import java.util.Arrays;
+import java.util.List;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
+
+
+public class SparkJavaTest {
+	
+	
+	
+	
+   
+
+    public static Seq<String> convertListToSeq(List<String> inputList) {
+        return JavaConverters.asScalaIteratorConverter(inputList.iterator()).asScala().toSeq();
+    }
+
+    public static void main(String[] args) {
+    	
+    	System.setProperty("hadoop.home.dir", "C:/winutils");
+    	
+   	 Logger.getLogger("org").setLevel(Level.ERROR);
+   	 
+   	 
+ 	final SparkSession  spark = SparkSession
+             .builder()
+             .appName("JavaSparkTest")
+             .master("local")
+             .getOrCreate();
+   	 
+   	 
+        Dataset<Row> ds = spark.read().option("header",true).csv("C:/Users/vijayvergiya_sa/Desktop/Dataset/spark_file.csv");
+
+        ds.show();
+        List<String> columns = Arrays.asList("InvoiceNo","StockCode","Description");
+
+        //using selectExpr
+        ds.selectExpr(convertListToSeq(columns)).show(false);
+
+        //using select => this first column will be added to select
+        List<String> columns2 = Arrays.asList("StockCode","Description");
+
+        ds.select("InvoiceNo", convertListToSeq(columns2)).show(false);
+
+    }
+}
